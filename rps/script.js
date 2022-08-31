ab4051da53a168b1e9c5a2ab86beca9165ffd4c0
Mcyates <mcyatesvm@gmail.com>
2022-08-31 03:01:57 -0500

    finished rps

--- rps/script.js
+++ rps/script.js
@@ -1,57 +1,133 @@
-function game() {
-    let winner = " ";
-    let gameWin = 0;
-    let gameLoss = 0;
-    let gameRounds = 0;
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const outCome = document.querySelector('.outcome');
+const playerSelection = 0;
+const computerSelection = "";
+const p = document.createElement('p');
+let wins = 0;
+let losses = 0;
+let tiedGames = 0;
+const ties = document.querySelector('.tie');
+const roundWins = document.querySelector('.win');
+const roundLoss = document.querySelector('.loss');
 
-    //This function gets the computer to choose a random choice from the array and give it to us.
-    function getComputerChoice() {
-        let rps = [
-            "rock",
-            "paper",
-            "scissors"
-        ];
-        let computer = Math.floor(Math.random() * rps.length);
+roundWins.innerText = `Player: ${wins}`;
+roundLoss.innerText = `Opponent: ${losses}`;
+ties.innerText = `Tied: ${tiedGames}`;
+
+//This function gets the computer to choose a random choice from the array and give it to us.
+
+function getComputerChoice() {
+    let rps = [
+        "rock",
+        "paper",
+        "scissors"
+    ];
+    let computer = Math.floor(Math.random() * rps.length);
         let decision = rps[computer];
-        //console.log(decision);
-        return decision;
-    }
-    //This is the loop that causes the game to play through 5 rounds and then output a winner.
-    while(gameRounds < 5){
-        gameRounds++
-        function playRound() {
+    //console.log(decision);
+    return decision;
+};
 
-            let answer = prompt("Choose rock,paper,or scissors:");
-            let fixedAnswer = answer.toLowerCase();    
-            let computerSelection = getComputerChoice();
-            let playerSelection = fixedAnswer;
-        
-            if(playerSelection == "rock" && computerSelection == "scissors"){
-                gameWin++;
-            }else if(playerSelection == " rock " && computerSelection == "paper"){
-                gameLoss++;
-            }else if(playerSelection == "paper" && computerSelection == "rock"){
-                gameWin++;
-            }else if(playerSelection == "paper" && computerSelection == "scissors"){ 
-                gameLoss++;
-            }else if(playerSelection == "scissors" && computerSelection == "paper"){
-                gameWin++;
-            }else if(playerSelection == "scissors" && computerSelection == "rock"){
-                gameLoss++;
-            }else if(playerSelection == computerSelection){
-                return "Tie Game!";
-            }
-        }
-        //This call the imbedded function of playRound to start the loop.
-        playRound();
+
+    rock.addEventListener('click', () =>{
+        const computerSelection = getComputerChoice();
+        const playerSelection = "rock";
+        playRound(playerSelection,computerSelection);
+    });
+
+    paper.addEventListener('click', () =>{
+        const computerSelection = getComputerChoice();
+        const playerSelection = "paper";
+        playRound(playerSelection,computerSelection);
+    });
+
+    scissors.addEventListener('click', () =>{
+        const computerSelection = getComputerChoice();
+        const playerSelection = "scissors";
+        playRound(playerSelection,computerSelection);
+    });
+  
+const playRound = (playerSelection, computerSelection) => {
+    if(playerSelection == "rock" && computerSelection == "scissors"){
+        wins++;
+        p.innerText = "Rock beats Scissors congratulations\!";
+        outCome.appendChild(p);
+        roundWin();
+        winner();
+    }else if(playerSelection == "rock" && computerSelection == "paper"){
+        losses++;
+        p.innerText = "Rock loses to Paper sorry\!";
+        outCome.appendChild(p);
+        roundWin();
+        winner();
+    }else if(playerSelection == "paper" && computerSelection == "rock"){
+        wins++;
+        p.innerText = "Paper beats Rock congratulations\!";
+        outCome.appendChild(p);
+        roundWin();
+        winner();
+    }else if(playerSelection == "paper" && computerSelection == "scissors"){ 
+        losses++;
+        p.innerText = "Paper loses to Scissors sorry\!";
+        outCome.appendChild(p);
+        roundWin();
+        winner();
+    }else if(playerSelection == "scissors" && computerSelection == "paper"){
+        wins++;
+        p.innerText = "Scissors beats paper congratulations\!";
+        outCome.appendChild(p);
+        roundWin();
+        winner();
+    }else if(playerSelection == "scissors" && computerSelection == "rock"){
+        losses++;
+        p.innerText = "Scissors loses to Rock sorry\!";
+        outCome.appendChild(p);
+        roundWin();
+        winner();
+    }else if(playerSelection == computerSelection){
+        tiedGames++;
+        p.innerText = "Tie Game\!";
+        outCome.appendChild(p);
+        roundWin();
+        winner();
     }
-    //This checks to see if the player won or lost the game by examing the variables listed.
-    if(gameWin > gameLoss){
-        winner = " Victory is yours!";
-    }else if(gameLoss > gameWin) {
-        winner = " You have lost!"
+    
+};
+
+const roundWin = () => {
+    roundWins.innerText = `Player: ${wins}`;
+    roundLoss.innerText = `Opponent: ${losses}`;
+    ties.innerText = `Tied: ${tiedGames}`;
+};
+    
+const winner = () => {
+    if (wins < 5 && losses < 5 && tiedGames < 5){
+        return;
+    }else if(wins == 5){
+        p.innerText = "Player has won\!"
+        outCome.appendChild(p);
+        wins = 0;
+        losses = 0;
+        tiedGames = 0;
+        roundWin();
+    }else if(losses == 5){
+        p.innerText = "Computer has won\!"
+        outCome.appendChild(p);
+        wins = 0;
+        losses = 0;
+        tiedGames = 0;
+        roundWin();
+    }else if(tiedGames == 5){
+        p.innerText = "Tie Game maybe next time\!"
+        outCome.appendChild(p);
+        wins = 0;
+        losses = 0;
+        tiedGames = 0;
+        roundWin();
     }
-    return winner;
-}
- 
-console.log(game());
+};
+
+
+    
